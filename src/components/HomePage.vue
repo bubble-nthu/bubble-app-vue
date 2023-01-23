b220749314caca2f462b6c838d64754c26d801a6
Huang Chuan <river1440>
2023-01-23 23:29:38 +0800

    feat: add basic layout to homepage; add search bar to homepage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Header from './BasicLayout/Header.vue'
+import SearchBar from './Function/SearchBar.vue'
+
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <Header />
+      </el-header>
+      <el-main>
+        <SearchBar />
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+<style>
+</style>
