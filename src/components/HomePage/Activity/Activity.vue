43367c5bfd0f6a3aa992efbad0c2dd2f7f15ff3a
Huang Chuan <river1440>
2023-01-25 00:36:44 +0800

    feat: add Card; Modify Header

---
+++ src/components/HomePage/Activity/Activity.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Card from '../../Function/Card.vue'
+
+</script>
+
+<template>
+  <el-row style="padding: 20px">
+    <el-col
+      v-for="(o, index) in 6"
+      :key="o"
+      :span="6"
+      :xs="12" :sm="8"
+      style="display: flex"
+    >
+      <Card />
+    </el-col>
+  </el-row>
+</template>
+
+<style>
+.el-row {
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 20px;
+}
+.el-row:last-child {
+  margin-bottom: 0;
+}
+.el-col {
+  border-radius: 16px;
+}
+
+.time {
+  font-size: 12px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.button {
+  padding: 0;
+  min-height: auto;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+</style>
---
+++ src/components/HomePage/HomePage.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import Header from '../BasicLayout/Header.vue'
+import SearchBar from '../Function/SearchBar.vue'
+import Activity from './Activity/Activity.vue'
+
+</script>
+
+<template>
+  <Header />
+  <section class="main-wrapper">
+    <Activity />
+  </section>
+  <el-footer>Footer</el-footer>
+</template>
+<style>
+.main-wrapper {
+  display: flex;
+  margin: 1rem 8rem;
+  height: 100%;
+  width: 100%;
+}
+</style>
