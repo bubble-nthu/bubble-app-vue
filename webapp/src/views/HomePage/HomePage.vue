8cf0b6ef39cc3ae4e9fd4faed8fda8b09dd23fbc
Huang Chuan <river1440>
2023-02-04 22:57:24 +0800

    chore: add recommendation, function and search back.

--- webapp/src/views/HomePage/HomePage.vue
+++ webapp/src/views/HomePage/HomePage.vue
@@ -2,11 +2,16 @@
 import { ref } from 'vue'
 import Header from '@/components/BasicLayout/Header.vue'
 import Footer from '@/components/BasicLayout/Footer.vue'
+import SearchBar from '@/components/Function/SearchBar.vue'
+import Activity from '@/views/HomePage/Recommendation/Activity.vue'
+import Project from '@/views/HomePage/Recommendation/Project.vue'
 
 </script>
-
 <template>
   <Header />
+  <SearchBar />
+  <Activity />
+  <Project />
   <Footer />
 </template>
 <style>
---
+++ webapp/src/views/SearchResult/SearchResultView.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+import SearchResultBar from '@/views/SearchResult/SearchResultBar.vue'
+import SearchResultList from '@/views/SearchResult/SearchResultList.vue'
+
+</script>
+
+<template>
+  <section>
+    <SearchResultBar />
+    <SearchResultList />
+  </section>
+</template>
+
+<style>
+</style>
